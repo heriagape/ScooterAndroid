c9f50f0e12849815c19657d09ec19c19acef414e
HA <heri@heri.co>
2021-02-13 13:18:12 +0300

    Initial commit

---
+++ app/src/main/java/co/heri/scooter/MapsActivity.kt
@@ -0,0 +1,199 @@
+package co.heri.scooter
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.location.Location
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.card.MaterialCardView
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationSource.OnLocationChangedListener {
+
+    private lateinit var mMap: GoogleMap
+    // The entry point to the Fused Location Provider.
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val defaultLocation = LatLng(-33.8523341, 151.2106085)
+    private var locationPermissionGranted = false
+    private var lastKnownLocation: Location? = null
+    private var myLocation: LatLng? = null
+    private var theLocation: Location? = null;
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<*>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        // get the bottom sheet view
+        // get the bottom sheet view
+        val llBottomSheet = findViewById<MaterialCardView>(R.id.bottom_sheet)
+
+        // init the bottom sheet behavior
+        bottomSheetBehavior  =
+            BottomSheetBehavior.from(llBottomSheet)
+
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        this.setMapStyle()
+
+        // Turn on the My Location layer and the related control on the map.
+        this.updateLocationUI()
+
+        // Get the current location of the device and set the position of the map.
+        this.getDeviceLocation()
+
+    }
+
+    private fun setMapStyle() {
+        try {
+            mMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.map_style))
+        } catch (e: Resources.NotFoundException) {
+            Log.e("MapsActivity", "Cannot find style.", e)
+        }
+    }
+
+    private fun getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.applicationContext,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        this.locationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    this.locationPermissionGranted = true
+                }
+            }
+        }
+        this.updateLocationUI()
+    }
+
+    private fun updateLocationUI() {
+        try {
+            if (locationPermissionGranted) {
+                mMap.isMyLocationEnabled = true
+                mMap.uiSettings?.isMyLocationButtonEnabled = true
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            } else {
+                mMap.isMyLocationEnabled = false
+                mMap.uiSettings?.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                this.getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    private fun getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    LatLng(lastKnownLocation!!.latitude,
+                                            lastKnownLocation!!.longitude), DEFAULT_ZOOM.toFloat()))
+
+                            // Add a marker in Sydney and move the camera
+                            /*myLocation = LatLng(lastKnownLocation!!.latitude,
+                                    lastKnownLocation!!.longitude)
+                            mMap.addMarker(MarkerOptions().position(myLocation!!).title("My Locaton"))*/
+
+                             this.updateLocationUI()
+
+                        }
+                    } else {
+                        Log.d(TAG, "Current location is null. Using defaults.")
+                        Log.e(TAG, "Exception: %s", task.exception)
+                        mMap.moveCamera(CameraUpdateFactory
+                                .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat()))
+                        mMap.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    companion object {
+        private val TAG = MapsActivity::class.java.simpleName
+        private const val DEFAULT_ZOOM = 15
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+
+        // Keys for storing activity state.
+        private const val KEY_CAMERA_POSITION = "camera_position"
+        private const val KEY_LOCATION = "location"
+
+        // Used for selecting the current place.
+        private const val M_MAX_ENTRIES = 5
+    }
+
+    override fun onLocationChanged(location: Location?) {
+//        TODO("Not yet implemented")
+        if (location != null) {
+            myLocation = LatLng(location.getLatitude(), location.getLongitude())
+            // mMap.addMarker(MarkerOptions().position(myLocation!!).title("It's Me!"))
+        }
+    }
+
+}
